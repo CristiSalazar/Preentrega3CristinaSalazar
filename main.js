d7276da5b6a8050645a10b155daae581add694ae
CristiSalazar <cristina.salazar125@gmail.com>
2023-05-30 20:25:10 -0400

    carrito de compra

--- main.js
+++ main.js
@@ -56,11 +56,47 @@ lista.forEach(element => {
     console.log(carrito)
 });
 
+const tablaResumenC = document.getElementById("tabla")
 const verCarrito = document.getElementById("verCarrito")
 
 verCarrito.addEventListener("click", () =>{
-    console.log("probando")
+    const tablaResumen = document.createElement("div")
+    tablaResumen.innerHTML = `
+    <h3>Carrito</h3>
+    `
+    tablaResumenC.append(tablaResumen);
+    const tablaButton = document.createElement("h1")
+    tablaButton.innerText = "x";
+    tablaResumen.append(tablaButton);
+
+    carrito.forEach((element)=>{
+        let carritoContent = document.createElement("div")
+        carritoContent.innerHTML = `
+        <h3>${element.nombre}</h3>
+        <p>${element.precio}</p>
+        <p>${element.stock}</p>
+        `
+        tablaResumenC.append(carritoContent)
+    }
+    )
+
+    const total = carrito.reduce((ac, el) => ac + el.precio, 0);
+    const totalCompra = document.createElement("div")
+    totalCompra.innerHTML = `
+    Total a pagar: ${total}
+    `
+    tablaResumenC.append(totalCompra)
+
+
+    const stockRestante1 = carrito.reduce((ac, el) => ac - el.stock, 20);
+    const stockCompra = document.createElement("div")
+    stockCompra.innerHTML = `
+    Stock que queda: ${stockRestante1}
+    `
+    tablaResumenC.append(stockCompra)
+
 })
+
 
 
 
@@ -72,50 +108,12 @@ function filtrar(){
     const input = document.getElementById('filtrarP').value
     let palabraClave =input.trim().toLowerCase()
     let resultado = lista.filter((producto)=> producto.nombre.toLowerCase().includes(palabraClave))
-    // if (resultado.length > 0){
-    //    alert("hola")
-    // }else{
-    //     alert("no se encontró ninguna coincidencia con: "+ palabraClave)
-    // }
-    if (resultado.length > 0) {
-        const container = document.createElement('div');
-        container.classList.add('card-container');
-  
-    
-        resultado.forEach((producto) => {
-          const card = document.createElement('div');
-          card.classList.add('card');
-    
-          const nombre = document.createElement('h2');
-          nombre.textContent = producto.nombre;
-          card.appendChild(nombre);
-    
-          const precio = document.createElement('p');
-          precio.textContent = `Precio: ${producto.precio}`;
-          card.appendChild(precio);
-    
-          const stock = document.createElement('p');
-          stock.textContent = `Stock: ${producto.stock}`;
-          card.appendChild(stock);
-          
-  
-  
-  
-  
-          const fotoProducto = document.createElement('img');
-          stock.textContent = `imagen: ${producto.img}`;
-          card.appendChild(img);
-  
-  
-          container.appendChild(card);
-        });
-    
-        body.appendChild(container);
-      } else {
-        alert('No se encontraron coincidencias');
-      }
+    if (resultado.length > 0){
+       alert("h")
+    }else{
+        alert("no se encontró ninguna coincidencia con: "+ palabraClave)
     }
-
+}
 
 
 // let stockRestante1a =[]
@@ -132,64 +130,8 @@ function filtrar(){
 // let suma2 = 0
 // let suma3 = 0
 
-//  while(continuar===true){
-//      tipoDeLibro = prompt("Seleccione el libro que desea comprar: " + listaNombres.join(", ") + ".").toLowerCase() 
-//      while(tipoDeLibro==="" || tipoDeLibro !== "alma contra alma" && tipoDeLibro !== "el quijote" && tipoDeLibro !== "las aventuras de mila"){
-//          tipoDeLibro = prompt("Lo sentimos, debe escoger un nombre válido: Alma contra alma, El Quijote, Las aventuras de Mila")
-//      }
 
-//      switch(tipoDeLibro){
-//          case producto1.nombre:
-//             libroUno= parseInt(prompt("El costo de " + producto1.nombre + " es de " + producto1.precio + " y nos quedan " + stockRestante1 + " en stock, ¿Cuántos desea comprar?"))
-//                 if(libroUno >= 1){
-//                     precioUno = multiplicacion(libroUno, 15000)
-//                     precioUnoFinal.push(precioUno)
-//                     suma1 = precioUnoFinal.reduce((a, b) => a + b, 0)
-//                     compra.push(producto1.nombre)
-//                     stockRestante1 = resta(stockRestante1, libroUno)
-//                     stockRestante1a.push(stockRestante1)
-//                 }else{
-//                     precioUno = 0
-//                     alert("No ingresamos su compra, gracias por su tiempo") 
-//                 }
-//          break
-//          case producto2.nombre:
-//              libroDos = parseInt(prompt("El costo de " + producto2.nombre + " es de " + producto2.precio + " y nos quedan " + stockRestante2 + " en stock, ¿Cuántos desea comprar?"))
-//              if(libroDos >= 1){
-//                  precioDos = multiplicacion(libroDos, 10000)
-//                  precioDosFinal.push(precioDos)
-//                  suma2 = precioDosFinal.reduce((a, b) => a + b, 0)
-//                  compra.push(producto2.nombre)
-//                  stockRestante2 = resta(stockRestante2, libroDos)
-//                  stockRestante2a.push(stockRestante2)
-//              }else{
-//                  precioDos=0
-//                  alert("No ingresamos su compra, gracias por su tiempo")
-//              }
-//          break
-//          case producto3.nombre:
-//              libroTres = parseInt(prompt("El costo de " + producto3.nombre + " es de " + producto3.precio + " y nos quedan " + stockRestante3 + " en stock, ¿Cuántos desea comprar?"))
-//              if(libroTres >=1){
-//                  precioTres = multiplicacion(libroTres, 7000)
-//                  precioTresFinal.push(precioTres)
-//                  suma3 = precioTresFinal.reduce((a, b) => a + b, 0)
-//                  compra.push(producto3.nombre)
-//                  stockRestante3 = resta(stockRestante3, libroTres)
-//                  stockRestante3a.push(stockRestante3)
-//              }else{
-//              precioTres = 0
-//              alert("No ingresamos su compra, gracias por su tiempo")
-//              }
-//          break
-//      }
-//      if (libroUno >=1 || libroDos >= 1 || libroTres >= 1 ){
-//          continuar = confirm("Su compra ha sido ingresada, desea agregar otro libro")
-//      }
-//  }
 
-// let sumar = function (a,b,c){
-//    return (a + b + c)
-// }
 
-// alert("Su compra consiste en el(los) libro(s): " + compra.join(", ") + " , con un costo total de compra de: " + sumar(suma1, suma2, suma3) + " pesos. Muchas gracias por su compra.")
 
+         
